17322cefbb036d01f652bafa0cd4c73ec519bef4
Neocryptoquant <neocryptoquant@gmail.com>
2025-07-04 14:33:35 +0100

    Converted spl_init to make use of gill. Need help with the final lap

--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -1,15 +1,100 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "..//../turbin3-wallet.json"
+// The task for today is to replace @solana/web3.js and other bundles with gill. For sake of continuity, I will preserve the original code in comments while making use of gill by the side.
+// I will appreciate PRs to help solve this problem. This program does all the steps from init to metadata to minting.
 
-// Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+// Using gill only.
+import { createSolanaClient, generateKeyPairSigner, SendAndConfirmTransactionWithSignersFunction, getExplorerLink, createTransaction, type KeyPairSigner, getMinimumBalanceForRentExemption, signTransactionMessageWithSigners, getSignatureFromTransaction, sendAndConfirmTransactionWithSignersFactory } from 'gill';
+import { loadKeypairSignerFromFile } from "gill/node";
+import { getCreateAccountInstruction, getCreateMetadataAccountV3Instruction, getTokenMetadataAddress } from 'gill/programs';
+import { TOKEN_2022_PROGRAM_ADDRESS, getInitializeMintInstruction, getMintSize } from 'gill/programs/token';
 
-//Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+//import { createMint } from '@solana/spl-token';
+//import wallet from "..//../turbin3-wallet.json"
+//import { Keypair, Connection, Commitment } from "@solana/web3.js";
+//import { Keypair, sendAndConfirmTransaction } from '@solana/web3.js';
+
+
+// using gill to allocate the necessary imports
+const {rpc, rpcSubscriptions} = createSolanaClient({urlOrMoniker: "devnet"});
+const tokenProgram = TOKEN_2022_PROGRAM_ADDRESS;
+const space = getMintSize();
+
 
 (async () => {
+    try {
+        
+        const {value: latestBlockhash} = await rpc.getLatestBlockhash().send();
+
+        const signer: KeyPairSigner = await loadKeypairSignerFromFile("../turbin3-wallet.json");
+        const mint = await generateKeyPairSigner();
+        const metadataAddress = await getTokenMetadataAddress(mint);
+
+        const transaction = createTransaction({
+            feePayer: signer,
+            version: "legacy",
+            instructions: [
+                getCreateAccountInstruction({
+                    space,
+                    lamports: getMinimumBalanceForRentExemption(space),
+                    newAccount: mint,
+                    payer: signer,
+                    programAddress: tokenProgram,
+                }),
+                getInitializeMintInstruction({
+                    mint: mint.address,
+                    mintAuthority: signer.address,
+                    freezeAuthority: signer.address,
+                    decimals: 9,
+                }, {
+                    programAddress:tokenProgram,
+                },),
+                getCreateMetadataAccountV3Instruction( {
+                    collectionDetails: null,
+                    isMutable: true,
+                    updateAuthority: signer,
+                    mint: mint.address,
+                    metadata: metadataAddress,
+                    mintAuthority: signer,
+                    payer: signer,
+                    data: {
+                        sellerFeeBasisPoints: 0,
+                        collection: null,
+                        creators: null,
+                        uses: null,
+                        name: "Shrinath Aritotelian",
+                        symbol:"SHA",
+                        uri: "https://gateway.irys.xyz/G9Q2eJ5EgyoA6EMcSTdsYJnWeCDzx2TQWjrBQbmPCKr2",
+                    },
+                }),
+            ],
+            latestBlockhash,
+        });
+        //  const sendAndConfirm = await sendAndConfirmTransactionWithSignersFactory({rpc,rpcSubscriptions});
+        // console.log(sendAndConfirm);
+
+        // signed the transactions successfully, but the problem is getting it to on-chain and 
+        const signedTransaction = await signTransactionMessageWithSigners(transaction);
+        console.log("\nCheck the Explorer for your TXN:", getExplorerLink({cluster: "devnet",
+            transaction: getSignatureFromTransaction(signedTransaction),
+        })
+        
+    )}
+    catch(err) {
+        console.log(`\n Something went wrong. Oops!: ${err}`)
+    }
+})()
+
+
+
+// The original spl_init continues here.
+
+// Import our keypair from the wallet file
+//const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+//const commitment: Commitment = "confirmed";
+//const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+/* (async () => {
     try {
         // Start here
         const mint = await createMint(connection, keypair, keypair.publicKey, null, 6);
@@ -18,3 +103,4 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
+*/
