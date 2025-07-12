91e0a77faa6b60d48b44cf24e958d12190a3fcc4
Neocryptoquant <neocryptoquant@gmail.com>
2025-07-12 19:24:51 +0100

    Updated state for my code to be uploaded to the repo

--- amm_q3/programs/amm_q3/src/error.rs
+++ amm_q3/programs/amm_q3/src/error.rs
@@ -1,4 +1,6 @@
-use anchor_lang::prelude::*;
+use anchor_lang::error_code;
+use constant_product_curve:: CurveError;
+//use constant_product
 
 #[error_code]
 pub enum ErrorCode {
--- amm_q3/programs/amm_q3/src/instructions/deposit.rs
+++ amm_q3/programs/amm_q3/src/instructions/deposit.rs
@@ -1,5 +1,6 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{associated::AssociatedToken, token::{Mint, Token, TokenAccount}};
+use anchor_lang::{prelude::*, init_if_needed};
+use anchor_spl::{associated::AssociatedToken, token::Mint, Token, TokenAccount};
+use constant_product_curve::ConstantProduct;
 
 use crate::{state::Config};
 
@@ -33,10 +34,18 @@ pub struct Deposit<'info> {
     )]
     pub vault_y: Account<'info, TokenAccount>,
 
+
     
     #[account(
         mut,
         associated_token::mint = mint_x,
+        associated_token::authority = user
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        associated_token::mint = mint_y.,
         associated_token::authority = user
     )]
     pub user_y: Account<'info, TokenAccount>,
@@ -50,9 +59,66 @@ pub struct Deposit<'info> {
     )]
     pub user_lp: Account<'info, TokenAccount>,
 
-    
+    pub config: Account<'info, Config>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>
 
+}
+
+impl<'info> Deposit<'info> {
+
+    pub fn deposit(
+        &mut self, 
+        amount: u64, 
+        max_x: u64, 
+        max_y: u64) -> Result<()> {
+        require!(self.config.locked == false, AmmError::PoolLocked);
+        require!(amount != 0, AmmError::InvalidAmount);
+        
+
+        let (x, y) = match self.mint_lp.supply == 0 && self.vault_x.amount == 0 && self.vault_y.amount == 0 {
+            true => (max_x, max_y),
+            false => {
+                let amount = ConstantProduct::xy_deposit_amounts_from_l(
+                    x: self.vault_x.amount,
+                    y: self.vault_y.amount,
+                    l: self.mint_lp.supply,
+                    a: amount,
+                precision: 6).unwrap();
+                (amount.x, amount.y)
+            }
+        };
+    }
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (self.user_x.to_account_info(), self.vault_x.to_account_info()),
+            false => (self.user_y.to_account_info(), self.vault_y.to_account_info()),
+        };
+
+        let cpi_program =  self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info()
+        };
+
+        let ctx = CpiContext::new(cpi_program, cpi_accounts);
+        transfer(ctx, amount)
+    }
+
+    pub fn mint_lp_tokens(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = MintTo {
+            mint: self.mint_lp.to_account_info(),
+            to: self.user_lp.to_account_info(),
+            authority: self.config.to_account_info()
+        };
+        let signer_seeds = &[&seeds[..]];
+        
+        let ctx = CpiContext::new_with_signer(self.token_program, cpi_accounts, signer_seeds);
+        mint_to(ctx, amount)
+    }
 }
